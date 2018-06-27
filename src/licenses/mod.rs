ebc83bb9118978ca64b48d902d49c2a14efee8f0
Mathew Robinson <chasinglogic@gmail.com>
2018-06-27 00:15:35 -0400

    Make year optional, make config derive serde_yaml

--- src/licenses/mod.rs
+++ src/licenses/mod.rs
@@ -3,11 +3,12 @@ use chrono::prelude::*;
 mod templates;
 use self::templates::*;
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
-    ident: String,
-    author: String,
-    year: String,
-    email: Option<String>,
+    pub ident: String,
+    pub author: String,
+    pub year: Option<String>,
+    pub email: Option<String>,
 }
 
 impl Config {
@@ -16,31 +17,40 @@ impl Config {
             ident: ident.to_string(),
             author: author.to_string(),
             email: None,
-            year: Local::now().year().to_string(),
+            year: Some(Local::now().year().to_string()),
         }
     }
 
-    pub fn with_email(mut self, email: String) -> Config {
-        self.email = Some(email);
+    pub fn with_email(mut self, email: &str) -> Config {
+        self.email = Some(email.to_string());
         self
     }
 
-    pub fn with_year(mut self, year: String) -> Config {
-        self.year = year;
+    pub fn with_year(mut self, year: &str) -> Config {
+        self.year = Some(year.to_string());
         self
     }
 
     pub fn render(&self) -> String {
-        let mut rendered = self.template()
-            .replace("{year}", &self.year)
+        let year = if let Some(ref y) = self.year.as_ref() {
+            y.to_string()
+        } else {
+            Local::now().year().to_string()
+        };
+
+        let mut rendered = self
+            .template()
+            .replace("{year}", &year)
             .replace("{author}", &self.author)
             .replace("{ident}", &self.ident);
 
         if let Some(ref email) = self.email {
-            let mut tmpl_email = "<".to_string();
+            let mut tmpl_email = " <".to_string();
             tmpl_email.push_str(&email);
             tmpl_email.push_str(">");
             rendered = rendered.replace("{email}", &tmpl_email);
+        } else {
+            rendered = rendered.replace("{email}", "");
         }
 
         rendered
@@ -86,9 +96,7 @@ mod tests {
     #[test]
     fn test_render_short_ident() {
         assert_eq!("Copyright 2018 Mathew Robinson <chasinglogic@gmail.com>. All rights reserved. Use of this source code is
-governed by the MIT license that can be found in the LICENSE file.
-
-",
+governed by the MIT license that can be found in the LICENSE file.",
             Config::new("MIT", "Mathew Robinson")
                 .with_email("chasinglogic@gmail.com")
                 .with_year("2018")
@@ -112,10 +120,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-",
-            Config::new("MIT", "Mathew Robinson")
+along with this program.  If not, see <http://www.gnu.org/licenses/>.",
+            Config::new("GPL-3.0", "Mathew Robinson")
                 .with_email("chasinglogic@gmail.com")
                 .with_year("2018")
                 .render()
