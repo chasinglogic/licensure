62eddfc6e7d4a7b7a01e45ce4d2cbb8c2010a22a
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-12-03 20:38:47 -0600

    fix: if already licensed ignore the trailing lines and whitespace

--- src/licensure.rs
+++ src/licensure.rs
@@ -42,6 +42,18 @@ impl Licensure {
                 continue;
             }
 
+            // if already licensed but the trailing lines/whitespace do not match
+            let content_trimmed = content.trim_end_matches(|c| c == '\n' || c == '\r' || c == ' ');
+            let header_trimmed = header.trim_end_matches(|c| c == '\n' || c == '\r' || c == ' ');
+            if content_trimmed.contains(header_trimmed) {
+                info!(
+                    "{} already licensed but the trailing lines/whitespace do not match",
+                    file
+                );
+                // ignore the trailing lines for now so it does not result in duplicate license headers
+                continue; // TODO fix the trailing whitespace or empty lines to match the template
+            }
+
             header.push_str(&content);
 
             if self.config.change_in_place {
