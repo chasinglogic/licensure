bf26cd64e4b1794c46c491446af1cf3e38c241e9
Mathew Robinson <mathew@chasinglogic.io>
2024-05-24 11:32:30 +0100

    chore: add license headers with licensure

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,3 +1,16 @@
+// Copyright (C) 2024 Mathew Robinson <chasinglogic@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+//
 use std::env;
 use std::fs::File;
 use std::io;
