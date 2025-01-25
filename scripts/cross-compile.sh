408f59924e4b300bfa5be5825a3dd3546f53ae7d
Mathew Robinson <mathew@chasinglogic.io>
2025-01-25 13:53:24 +0000

    chore: relicense for the new year using licensure

--- scripts/cross-compile.sh
+++ scripts/cross-compile.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+# Copyright (C) 2025 Mathew Robinson <chasinglogic@gmail.com>
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+#
 
 if [[ ! -x $(which cross 2>/dev/null) ]]; then
     echo "You need to install cross to use this script:"
--- src/config/comment.rs
+++ src/config/comment.rs
@@ -1,14 +1,14 @@
 use std::path::Path;
 
-// Copyright (C) 2024 Mathew Robinson <chasinglogic@gmail.com>
-//
+// Copyright (C) 2025 Mathew Robinson <chasinglogic@gmail.com>
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
 // Foundation, version 3.
 //
 // This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
 //
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
